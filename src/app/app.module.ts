64f3b28aeb01980e966631f2ed5b8f890d086b27
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2023-11-09 12:04:14 +0100

    Inicio navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { ButtonModule } from 'primeng/button';
-
-import { RippleModule } from 'primeng/ripple'; 
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,9 +12,9 @@ import { RippleModule } from 'primeng/ripple';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    ButtonModule,
-    RippleModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
