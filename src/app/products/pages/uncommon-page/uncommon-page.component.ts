7fc0f21a7f0c3ade57e20e15cdb0842110aaa39c
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2023-11-20 14:44:47 +0100

    Fin pipes genericos

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -7,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  //i18n Select
+  public name: string = 'Roberto';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  changeClient(): void {
+    this.name = 'Melisa';
+    this.gender = 'female'
+  }
+
+  // i18n Plural
+
+  public clients: string[] = ['Barri', 'Jorge', 'Victor', 'Dani', 'Roberto', 'Celia', 'Catalina'];
+
+  public clientsMap = {
+    '=0': 'no tenemos clientes esperando',
+    '=1': '1 cliente esperando',
+    'other': ' # clientes esperando'
+  }
+  deleteClient(): void {
+    this.clients.shift();
+  }
+
+  //keyValue Pipe
+
+  public person = {
+    name: 'Roberto',
+    age: 28,
+    address: 'Madrid, Españita'
+  }
+
+  //async Pipe
+  public myObservableTimer: Observable<number> = interval(2000);
+  public promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('tenemos data en la promesa')
+    }, 3500);
+  });
 }
